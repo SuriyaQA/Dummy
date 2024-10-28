88ab0749f32e54dd692f016b4c72f263e29cf8b2
SuriyaQA <suriyayantra@gmail.com>
2024-10-28 19:21:33 +0530

    hi

---
+++ mavenProject/src/test/java/Smoke/PrpgramTest.java
@@ -0,0 +1,13 @@
+package Smoke;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PrpgramTest {
+
+	@Test
+	public void testCase() {
+	
+		Reporter.log("Test Case Executed",true);
+	}
+}
